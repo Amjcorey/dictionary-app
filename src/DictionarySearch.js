43da78f749f80d3e989d8e93ec0ee96fe2b73564
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-28 10:14:20 -0800

    updated form and added functions to look up a searched word and then have it show in the alert. Added useState to make the value show in the handleresponse

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
+	let [keyword, setKeyword] = useState("");
+
 	function WordSearch(event) {
 		event.preventDefault();
-		alert("Searching...");
+		alert(
+			`Searching everywhere for the definition of ${keyword}, just a moment...`
+		);
+	}
+
+	function handleKeyWord(event) {
+		setKeyword(event.target.value);
 	}
 
 	return (
 		<div className="Dictionary">
 			<form onSubmit={WordSearch}>
-				<input type="seach" />
+				<input type="seach" onChange={handleKeyWord} />
 			</form>
 		</div>
 	);
