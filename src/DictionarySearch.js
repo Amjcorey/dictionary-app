24e95754529cdf7718ec2e0c569ed6423eb4587a
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-11-28 22:32:35 -0800

    added api and axios, created the handleresponse for when a word is searched (? I think that's right...))

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -5,11 +5,19 @@ import axios from "axios";
 export default function DictionarySearch() {
 	let [keyword, setKeyword] = useState("");
 
+	function handleRespons(response) {
+		console.log(response.data[0]);
+	}
+
 	function WordSearch(event) {
 		event.preventDefault();
 		alert(
 			`Searching everywhere for the definition of ${keyword}, just a moment...`
 		);
+
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/sunset\\${keyword}`;
+
+		axios.get(apiUrl).then(handleRespons);
 	}
 
 	function handleKeyWord(event) {
