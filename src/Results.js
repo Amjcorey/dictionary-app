9e726b7a91e3aa1d30b7f580a2a70edf3f6cde71
Amjcorey <76081915+Amjcorey@users.noreply.github.com>
2021-12-01 13:01:54 -0800

    got rid of that logo192 in manifest

--- src/Results.js
+++ src/Results.js
@@ -7,8 +7,9 @@ export default function Results(props) {
 		return (
 			<div className="Results">
 				<h2>{props.results.word} </h2>
-				<WordMeanings />
-				{props.results.word}
+				{props.results.meanings.map(function (meaning, index) {
+					return meaning.definitions[0];
+				})}
 			</div>
 		);
 	} else {
